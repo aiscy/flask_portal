d65317d68b436a0056af3768d6db6c265155e686
aiscy <heher111@gmail.com>
2015-05-29 14:23:09 +0500

    скрипт для локального запуска

---
+++ run_portal.py
@@ -0,0 +1,12 @@
+__author__ = 'pavlomv'
+import os
+
+
+def main():
+    os.system(r'\\testkomp\python34$\python.exe ' +
+              os.path.join(os.path.dirname(os.path.realpath(__file__))
+                           , 'flask_portal.py'))
+
+
+if __name__ == '__main__':
+    main()
