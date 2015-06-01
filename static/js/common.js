72600341c09deac30ff87669c7f58d86c33238d0
hmelnov <pavelhmelnov@gmail.com>
2015-06-01 16:59:24 +0500

    Обновлена разметка обеденного меню, добавлена форма отправки

--- static/js/common.js
+++ static/js/common.js
@@ -29,47 +29,66 @@ $(document).ready(function () {
 		$("#button_translate").attr("href", "javascript:translate('" + sourceLang + "', '" + destLang + "', 'textarea#original', 'textarea#translate');");
 	});
 
-	// парсинг обеденного меню
+	// Разметка обеденного меню
 var TABLE = {};
 TABLE.formwork = function (table) {
 	var $tables = $(table);
 
 	$tables.each(function () {
 		var _table = $(this);
-
 				_table
 					.find('tbody tr:has("td strong")')
 					.append($('<td></td>'));
-
 				_table
 					.find('tbody tr').not(':has("td strong")')
-					.append($('<td class="edit"><input class="btn btn-default btn-order" type="button" value="Заказать"/></td>'))
-
+					.append('<td class="edit"><button class="btn btn-default btn-order" type="button">Заказать  <span class="badge"></span></button></td>');
+				_table.find('tbody tr').has('td strong:contains("Комплекс")').find('td:last-child').addClass('edit').html('<button class="btn btn-default btn-order" type="button">Заказать  <span class="badge"></span></button>');
+				_table.find('tbody tr').has('td strong:contains("Комплекс")').nextAll().find('td:last-child').html('<td></td>');
+				_table.find('tbody tr').has("td:first-child:empty").find('td:last-child').removeClass('edit').html('<td></td>');
 	});
 };
 TABLE.formwork('.table');
-	
-	$('.btn-order').click(function() {
-		var food = '';
-		food = $(this).parents('tr').find('td:first-child').text();
-		$(this).parents('tr').addClass('success');
-		$('#result').append($('<p>' + food +'</p>'));
-	});
+
+	// Событие на нажатие кнопки заказать
+		
+		$('.food_menu tr').has('td.edit').click(function() {
+		var count = $(this).find('.badge').text();
+		var food = $(this).find('td:first-child').text();
+		function addFood() {
+			$('#result').append($('<li>' + food +'<span></span></li>'));
+		};
+		if ($('#result li:contains("' + food + '")').text() = null) {
+			alert($('#result li:contains("' + food + '")').text());
+			};
+		$(this).addClass('success');
+		$(this).find('.badge').text(++count);
+
+			addFood ();
+	}); //||
+//	$('.btn-order').click(function() {
+//		var food = '';
+//		food = $(this).parents('tr').find('td:first-child').text();
+//		$(this).parents('tr').addClass('success');
+//			addFood();
+//	});
+
 	
 	// Отправка заказа
 
-
 $('#submit').click(function () {
-		var body_message = $('#result').text();
-var email = 'maxim_pavlov@uraltep.ru';
-var subject = 'Заказ';
+		var n=1;
+		var cabinet = $('#InputCabinet').val();
+		var initials = $('#InputInitials').val();
+		var body_message = 'Кабинет №' + cabinet + ' ' + initials + '%0D%0D';
+		$('#result li').each(function() {
+			body_message += n + '. ' + $(this).text() + '%0D';
+			n += 1;
+		});
+		var email = 'maxim_pavlov@uraltep.ru';
+		var subject = 'ПИЦ УралТЭП Заказ';
 			var mailto_link = 'mailto:' + email + '?subject=' + subject + '&body=' + body_message;
-
-			win = window.open(mailto_link, 'emailWindow');
-			if (win && win.open && !win.closed) win.close();
+		$('#test').attr('href', mailto_link);
+		win = window.open(mailto_link, 'emailWindow');
+		if (win && win.open && !win.closed) win.close();
 });
 });
-// $(window).load(function() {
-// 	$(".loader_inner").fadeOut();
-// 	$(".loader").delay(400).fadeOut("slow");
-// });
