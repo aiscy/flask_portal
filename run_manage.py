c62be7457e659dcb1289cfe2cfcf58d2fb60d6c3
aiscy <heher111@gmail.com>
2015-06-05 11:18:56 +0500

    подправил перенос запятой

--- run_manage.py
+++ run_manage.py
@@ -4,8 +4,8 @@ import os
 
 def main():
     os.system(r'\\testkomp\python34$\python.exe ' +
-              os.path.join(os.path.dirname(os.path.realpath(__file__))
-                           , 'manage.py ') + input())
+              os.path.join(os.path.dirname(os.path.realpath(__file__)),
+                           'manage.py ') + input())
 
 
 if __name__ == '__main__':
