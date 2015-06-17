3f8d04df2901d7bea521013fd9a8b5b419e5dc49
MaxPavlov <maxpav137@gmail.com>
2015-06-17 23:40:02 +0500

    Попытка прирвести дурацкую таблицу, полученную посредством конвертации ворда в хтмл, чтобы потом отдать ее angular

--- menu_test.py
+++ menu_test.py
@@ -3,23 +3,22 @@ from grab import Grab
 from lxml import html
 from lxml.html import clean
 from lxml.html import builder as E
-
+import re
 
 db = None
 g = Grab()
 g.go('http://www.privet-bufet.ru/article/menyu-na-segodnya')
 request = g.doc.select('//div[@class="panda-article"]').html()
 doc = html.document_fromstring(request)
+
+cleaner = clean.Cleaner(style=True, remove_tags=['p', 'em'])
+doc = cleaner.clean_html(doc)
 table = []
 for row in doc.cssselect("tr"):
     tds = row.cssselect("td")
-    for col in tds:
-        print(col.text_content())
-    print('-----')
-
-# cleaner = clean.Cleaner(style=True, remove_tags=('p', 'em'))
-# doc = cleaner.clean_html(doc)
-# html_new = html.tostring(E.DIV(E.TABLE(E.CLASS("table table-striped table-hover food_menu"), doc.cssselect('tbody')[-1])),
-#                          encoding='unicode')
-# # print(html_new)
+    table.append([tds[0].text_content(), tds[1].text_content(), tds[2].text_content()])
+for item in table:
+    print(item)
+    # for element in item:
+        # print(' '.join(element.split()))
 
