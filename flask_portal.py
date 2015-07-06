c831594c6dd209b903ed2081bcb683a6361c074c
aiscy <heher111@gmail.com>
2015-07-06 10:27:50 +0500

    Исправлены слеши, добавлен сервис rest api для angularjs

--- flask_portal.py
+++ flask_portal.py
@@ -1,14 +1,13 @@
-# все импорты
 import sqlite3
-import os
-import flask_sijax
+# import flask_sijax
 from flask import Flask, request, session, g, redirect, url_for, \
-                  abort, render_template, flash, jsonify, Response
+    abort, render_template, flash, jsonify, Response
 from yandex_translate import YandexTranslate
-# from py.sqlite_conn import connect_db
+from models import FoodMenu, db
 
 app = Flask(__name__)
 app.config.from_pyfile('configuration.py')
+db.init_app(app)
 yandex_translate = YandexTranslate(app.config['API_YANDEX_TRANSLATE_KEY'])
 # app.config['SIJAX_STATIC_PATH'] = os.path.join('.', os.path.dirname(__file__), 'static/js/sijax/')
 # app.config['SIJAX_JSON_URI'] = '/static/js/sijax/json2.js'
@@ -37,12 +36,12 @@ def close_db(error):
 def auth():
     if not session.get('logged_in'):
         if request.environ.get('REMOTE_USER'):
-            session['username'] = request.environ.get('REMOTE_USER')
+            session['user_name'] = request.environ.get('REMOTE_USER')
             session['logged_in'] = True
             flash('Вы успешно авторизировались')
         else:
             if app.debug:
-                session['username'] = 'debuguser'
+                session['user_name'] = 'DebugUser'
                 session['logged_in'] = True
                 flash('Вы авторизировались как {}'.format(session.get('username')))
             else:
@@ -57,19 +56,12 @@ def index():
     return render_template('index.html', quote_text=cur[0], quote_author=cur[1])
 
 
-@app.route('/service/food_menu')
+@app.route('/service/food_menu/')
 def food_menu():
     db = get_db()
     html = list(db.execute('SELECT html FROM food_menu').fetchone())
     # app.logger.debug(html)
     return render_template('food_menu.html', menu=html[0])
-
-
-@app.route('/service/food_menu.json')
-def food_menu_json():
-    with open('file.json', 'r') as file:
-        return Response(response=file.read(), status=200, mimetype="application/json")
-
 
 
 @app.route('/service/_translate', methods=['POST'])
@@ -90,21 +82,23 @@ def process_translate():
                         'auto_lang': translated.get('lang').lower().split('-')[0]})
 
 
-@app.route('/service/translate', methods=['GET'])
+@app.route('/service/translate/', methods=['GET'])
 def translate():
     db = get_db()
     yandex_lang_list = list(db.execute('SELECT * FROM yandex_lang ORDER BY language_full'))
     return render_template('translate.html', yandex_lang_list=yandex_lang_list)
 
 
-app.route('/service/visio')
-def service_visio():
-    return 1
-
-
-# app.route('/favicon.ico')
-# def favicon():
-#     return url_for('static', filename='favicon.ico')
+# Набросок RESTful API
+@app.route('/api/v1/service/foodmenu', methods=['GET'])
+def food_menu_json():
+    req = FoodMenu.query.get(1)
+    return jsonify(dict(
+        code=200,
+        date=req.date,
+        menu=req.menu,
+        menu_complex=req.menu_complex
+    ))
 
 
 if __name__ == '__main__':
